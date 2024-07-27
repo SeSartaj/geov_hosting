be37e4ce739439b86efedf7e69664105da60aa3b
sartaj Ahmad <sartajahmad1529@gmail.com>
2024-07-27 10:57:14 +0430

    make map width responsive and add full screen controll

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import 'maplibre-gl/dist/maplibre-gl.css';
 import Sidebar from './components/Sidebar';
-import { MapContext, MapProvider } from './contexts/MapContext';
+import { MapProvider } from './contexts/MapContext';
 import MyMap from './components/MyMap';
+import { FullscreenControl } from 'react-map-gl';
+{
+  FullscreenControl;
+}
 
 function App() {
   return (
     <MapProvider>
-      <MyMap />
-      <Sidebar />
+      <div style={{ padding: 30, marginTop: 20 }}>
+        <MyMap>
+          <FullscreenControl position='top-right' />
+          <Sidebar />
+        </MyMap>
+      </div>
     </MapProvider>
   );
 }
--- src/components/MyMap/index.jsx
+++ src/components/MyMap/index.jsx
@@ -1,6 +1,6 @@
-import Map, { useMap } from 'react-map-gl/maplibre';
+import Map, { useMap, useControl } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { MapContext } from '../../contexts/MapContext';
 
 export default function MyMap({ children, props }) {
@@ -14,7 +14,7 @@ export default function MyMap({ children, props }) {
         latitude: 34.5553,
         zoom: 3,
       }}
-      style={{ width: window.innerWidth, height: window.innerHeight }}
+      style={{ width: '100%', height: '80vh' }}
       mapStyle={typeof mapStyle === 'string' ? mapStyle : mapStyle.toJS()}
       attributionControl={false}
       reuseMaps
